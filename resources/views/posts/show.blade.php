409e9fb04c185b3e7e8d084217882c087aa46e29
david <dpark543107@gmail.com>
2017-02-11 13:40:30 -0600

    post grabbing info from db

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('content')
+    <div class="col-sm-8 blog-main">
+        <div class="blog-post">
+            <h2 class="blog-post-title">{{ $post->title }}</h2>
+            <p class="blog-post-meta">
+                {{ $post->created_at }} by <a href="#">Mark</a>
+            </p>
+
+            <p>{{ $post->body }}</p>
+        </div>
+    </div>
+@endsection
